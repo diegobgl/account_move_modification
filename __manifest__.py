f35caa82af7103f5b386a4369fe0e1d4f67775e1
Diego Gajardo <deskodoo@a2group.cl>
2024-07-22 14:01:58 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Account Move Modification',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Modifica la función en account.move eliminando una condición',
+    'description': 'Este módulo modifica y hereda la función en account.move para eliminar una condición específica.',
+    'author': 'I+D, Diego Gajardo, Camilo Neira, Diego Morales',
+    'website': 'https://www.holdconet.com',
+    'depends': ['account'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
